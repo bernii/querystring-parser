33b7f6090543576262f7babb6aa6cbaa9089c91a
berni <berni@extensa.pl>
2011-06-07 17:20:34 +0200

    Added email to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='querystring_parser',
       version='1.0',
       description='QueryString parser for Python/Django that correctly handles nested dictionaries',
       author='bernii',
-      author_email='',
+      author_email='berni@extensa.pl',
       url='https://github.com/bernii/querystring-parser',
       packages=['querystring_parser'],
 )
