a13048ea810adbddc9c2d24945273835c4d463b4
Abhinav <in.abhi9@gmail.com>
2014-03-02 21:25:46 +0530

    change test case subject

--- querystring_parser/tests.py
+++ querystring_parser/tests.py
@@ -143,16 +143,35 @@ class BuilderAndParser(unittest.TestCase):
 class NormalizedParse(unittest.TestCase):
     '''
     '''
-    knownValues = KnownValues.knownValues
-    knownValuesNormalized = KnownValues.knownValues
-    knownValuesNormalized['section'] = [{u'words': [[u'sdfsd', u'ds'],
-                                                    [u'', u''], [u'', u'']]}]
+    knownValues = {"section": {10: {u"words": {-1: [u"", u""], -2: [u"", u""],
+                                               30: [u"noga", u"leg"]},
+                                    "name": u"sekcja siatkarska"},
+                                  11: {u"words": {-1: [u"", u""],
+                                                  -2: [u"", u""],
+                                                  31: [u"renca", u"rukka"]},
+                                      u"del_words": {32: [u"kciuk", u"thimb"],
+                                                     33: [u"oko", u"an eye"]},
+                                      u"name": u"sekcja siatkarska1"},
+                                  12: {u"words": {-1: [u"", u""],
+                                                  -2: [u"", u""],
+                                                  34: [u"wlos", u"a hair"]},
+                                      u"name": u"sekcja siatkarska2"}}}
+    knownValuesNormalized = {'section':
+                                [{'name': 'sekcja siatkarska',
+                                  'words': [['', ''], ['', ''],
+                                            ['noga', 'leg']]},
+                                 {'del_words': [['kciuk', 'thimb'],
+                                                ['oko', 'an eye']],
+                                  'name': 'sekcja siatkarska1',
+                                  'words': [['', ''], ['', ''],
+                                            ['renca', 'rukka']]},
+                                 {'name': 'sekcja siatkarska2',
+                                  'words': [['wlos', 'a hair'], ['', ''],
+                                            ['', '']]}]}
 
     def test_parse_normalized(self):
-
-        for key, dic in KnownValues.knownValues.items():
-            result = parse(build(dic), normalized=True)
-            self.assertEqual(self.knownValuesNormalized[key], result)
+        result = parse(build(self.knownValues), normalized=True)
+        self.assertEqual(self.knownValuesNormalized, result)
 
 if __name__ == "__main__":
     unittest.main()
