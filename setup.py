df76803407ad860df16b6a984e0767409b8bae5c
Bernard Kobos <berni@extensa.pl>
2014-03-27 19:40:56 +0100

    actually let's use SEMVER

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='querystring_parser',
-      version='1.1.1',
+      version='1.2.0',
       description='QueryString parser for Python/Django that correctly handles nested dictionaries',
       author='bernii',
       author_email='berni@extensa.pl',
