bbff5636a491b585d60eb9802c4d2adf982397ac
Zeng Ohm <zengohm@gmail.com>
2014-05-17 12:04:26 +0800

    be suitable for multidimensional arrays in jquery
    Make it suitable for multidimensional arrays in jquery ajax,  just like:
    Form Data:
    biz[0][id]:something001
    biz[0][post][]:photo001_001
    biz[0][post][]:photo001_002
    biz[1][id]:something002
    biz[1][post][]:photo002_001
    biz[1][post][]:photo002_002

--- querystring_parser/parser.py
+++ querystring_parser/parser.py
@@ -128,10 +128,10 @@ def parse(query_string, unquote=True, encoding='utf-8'):
         while k in tempdict and type(v) is dict:
             tempdict = tempdict[k]
             (k, v) = v.popitem()
-        if k in tempdict and type(tempdict[k]).__name__ == 'list':
-            tempdict[k].append(v)
-        elif k in tempdict:
-            tempdict[k] = [tempdict[k], v]
+        if type(tempdict) == list and k == '':
+            tempdict.append(v)
+        elif type(v) is dict and v.has_key(''):
+            tempdict[k] = [v['']]
         else:
             tempdict[k] = v
     return mydict
