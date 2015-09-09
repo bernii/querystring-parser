6bd45faa359516c19fdd76c4396c160f0ee67da6
bernii <bkobos@gmail.com>
2015-09-09 23:40:20 +0200

    bump version number after PR merge

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='querystring_parser',
-      version='1.2.0',
+      version='1.2.1',
       description='QueryString parser for Python/Django that correctly handles nested dictionaries',
       author='bernii',
       author_email='berni@extensa.pl',
