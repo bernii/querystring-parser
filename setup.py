1d3b652512d55622a37b5f5712909ea41490454b
Paul Melnikow <email@paulmelnikow.com>
2019-04-07 21:02:56 -0500

    Check in the license and note in pypi classifiers

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 
 from distutils.core import setup
 
-setup(name='querystring_parser',
-      version='1.2.3',
-      description='QueryString parser for Python/Django that correctly handles nested dictionaries',
-      author='bernii',
-      author_email='berni@extensa.pl',
-      url='https://github.com/bernii/querystring-parser',
-      packages=['querystring_parser'],
-      install_requires=[
-        "requests",
-        "six",
-	  ],
+setup(
+    name="querystring_parser",
+    version="1.2.3",
+    description="QueryString parser for Python/Django that correctly handles nested dictionaries",
+    author="bernii",
+    author_email="berni@extensa.pl",
+    url="https://github.com/bernii/querystring-parser",
+    packages=["querystring_parser"],
+    install_requires=["requests", "six"],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
 )
