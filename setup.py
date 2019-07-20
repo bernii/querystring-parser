570470d2325909ccbfc62d6f1e8489526559bb92
Paul Melnikow <github@paulmelnikow.com>
2019-07-20 10:50:53 -0500

    RELEASE 1.2.4 (#34)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name="querystring_parser",
-    version="1.2.3",
+    version="1.2.4",
     description="QueryString parser for Python/Django that correctly handles nested dictionaries",
     author="bernii",
     author_email="berni@extensa.pl",
